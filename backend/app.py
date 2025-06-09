e436087d18b69350c006ff6c8bc815333243ab23
raul <raulnavase@gmail.com>
2025-06-09 20:51:43 +0200

    added config for render

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ def create_app():
 
     app = Flask(__name__)
 
-    CORS(app, supports_credentials=True, origins="*")
+    CORS(app, supports_credentials=True, origins=["https://tfg-dog-go.vercel.app"])
 
     app.config["MONGO_URI"] = os.getenv("MONGO_URI")
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
